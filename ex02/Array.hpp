cbb6ec6656e9ee0fc8de5ff11ca1f32a85988cea
Ahmed Mokhtari <mokhtari.ahmed21@ump.ac.ma>
2025-02-05 17:48:50 +0100

    ex02

---
+++ ex02/Array.hpp
@@ -0,0 +1,78 @@
+#include <stdexcept>
+
+template <typename T>
+class Array{
+    private:
+        T *arr;
+        unsigned int len;
+    public:
+        Array(){
+            arr = NULL;
+            len = 0;
+        }
+
+        Array(unsigned int n){
+            arr = NULL;
+            if (n <= 0)
+                throw 5;
+            try{
+                arr = new T[n]; 
+            }
+            catch (std::bad_alloc &e)
+            {
+                std::cerr << e.what() << std::endl;
+            }
+            len = n;
+        }
+
+        ~Array(){
+            delete[] arr;
+        }
+
+        Array(const Array &cp){
+            try{
+                this->arr = new T[cp.len];
+                this->len = cp.len;
+                for (unsigned int i = 0; i < len; i++)
+                    arr[i] = cp.arr[i];
+            }catch (std::bad_alloc &e)
+            {
+                std::cerr << e.what() << std::endl;
+            }
+        };
+
+        Array &operator=(const Array &cp){
+            if (this != &cp)
+            {
+                delete []arr;
+                try{
+                    this->arr = new T[cp.len];
+                    this->len = cp.len;
+                    for (unsigned int i = 0; i < len; i++)
+                        arr[i] = cp.arr[i];
+                }catch (std::bad_alloc &e)
+                {
+                    std::cerr << e.what() << std::endl;
+                }
+            }
+            return *this;
+        }
+
+        T& operator[](unsigned int idx)
+        {
+            if (idx >= len)
+                throw std::out_of_range("index out of rangeeee");
+            return arr[idx];
+        }
+        size_t size() const{
+            return len;
+        }
+};
+
+
+
+        // Array();
+        // Array(unsigned int n);
+        // Ãrray();
+        // Array &Array(const Array &cp);
+        // Array &operator=(const Array &cp);
---
+++ ex02/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Array.hpp"
+
+int main( void ) {
+    Array<int> arr(5);
+    Array<std::string> arrStr(10);
+    for (unsigned int i = 0;  i < arr.size() ; i++)
+        arr[i] = i;
+    for (unsigned  int i = 0;  i < arrStr.size() ; i++)
+        arrStr[i] = "ALo test";
+
+
+     for (unsigned int i = 0;  i < arrStr.size() ; i++)
+        std::cout << arrStr[i] << " ";
+    std::cout << std::endl;
+    Array<int> arr1 = arr;
+
+    Array<int> arr2;
+
+    arr2 = arr1;
+    for (unsigned int i = 0;  i < arr.size() ; i++)
+        std::cout << arr[i] << " " ;
+    std::cout << std::endl;
+    for (unsigned  int i = 0;  i < arr1.size() ; i++)
+        std::cout << arr1[i] << " " ;
+    std::cout << std::endl;
+    for (unsigned int i = 0;  i < arr2.size() ; i++)
+        std::cout << arr2[i] << " " ;
+    std::cout << std::endl;
+    try{
+        arr[5] = 22;
+    }catch(std::out_of_range &e){
+        std::cerr << e.what() << std::endl;
+    }
+    return 0;
+}
